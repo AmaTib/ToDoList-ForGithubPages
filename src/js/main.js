51d2a73b268824e94c24d2257a6ab862e77659b9
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2023-11-18 15:41:48 +0100

    La in min forloop som skapar html för 'att göra' listan i en funktionen: htmlForToDoList

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,74 @@
 import { Logger } from "sass";
 import "./../scss/style.scss";
 
-const toDoList = ["Wake up", "Eat", "Sleep", "Repeat"];
-const changedList = [];
+let toDoList = ["Wake up", "Eat", "Sleep", "Repeat"];
+const completedTasksList = [];
 
 const input = document.querySelector("input");
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 const completedList = document.getElementById("completedList");
 
-//placerar uppgifterna som en li i en ul lista
-toDoList.forEach((task, i) => {
-  const li = document.createElement("li");
-  const p = document.createElement("p");
-  const removeButton = document.createElement("button");
-  const liContainer = document.createElement("div");
-  removeButton.innerHTML = "Ta bort";
-  p.innerHTML = task;
-
-  liContainer.appendChild(p);
-  liContainer.appendChild(removeButton);
-  li.appendChild(liContainer);
-  taskList.appendChild(li);
-
-  //tar bort uppgift och lägger den i avklarat listan när man klickar på ta bort knappen
-  removeButton.addEventListener("click", () => {
-    toDoList.splice(task, i);
-
-    htmlForCompleted();
-
-    console.log(toDoList);
-  });
-
-  //skapar html för avklarat listan. Anropas när man klickar på ta bort knappen
-  function htmlForCompleted() {
+//skapar html för "att göra" listan. Anropas direkt under funktionen.
+function htmlForToDo() {
+  taskList.innerHTML = "";
+  //placerar uppgifterna som en li i en ul lista
+  toDoList.forEach((task, i) => {
     const li = document.createElement("li");
     const p = document.createElement("p");
     const removeButton = document.createElement("button");
     const liContainer = document.createElement("div");
-    removeButton.innerHTML = "Ångra";
+    removeButton.innerHTML = "Ta bort";
     p.innerHTML = task;
+
+    //tar bort uppgift från "att göra" och lägger den i "avklarat" listan när man klickar på ta bort knappen
+    removeButton.addEventListener("click", () => {
+      toDoList.splice(i, 1);
+      completedTasksList.push(task);
+      li.remove();
+      htmlForCompleted();
+    });
 
     liContainer.appendChild(p);
     liContainer.appendChild(removeButton);
     li.appendChild(liContainer);
+    taskList.appendChild(li);
+  });
+}
+htmlForToDo();
+
+//skapar html för avklarat listan. Anropas när man klickar på ta bort knappen
+function htmlForCompleted() {
+  completedList.innerHTML = "";
+
+  completedTasksList.forEach((removedTask, i) => {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    const undoButton = document.createElement("button");
+    const liContainer = document.createElement("div");
+    undoButton.innerHTML = "Ångra";
+    p.innerHTML = removedTask;
+
+    //klickevent för ångra knappen
+    undoButton.addEventListener("click", () => {
+      completedTasksList.splice(i, 1);
+      toDoList.push(removedTask);
+      htmlForCompleted();
+      htmlForToDo();
+    });
+
+    liContainer.appendChild(p);
+    liContainer.appendChild(undoButton);
+    li.appendChild(liContainer);
     completedList.appendChild(li);
-  }
-});
+
+    console.log(completedTasksList);
+    console.log(toDoList);
+  });
+}
 
 //försök att skapa klickevent för att lägga till uppgift
+//skapar nya uppgifter som läggs till i att göra listan (och i array) när man klickar på lägg till knappen
 addButton.addEventListener("click", () => {
   let newTask = input.value;
   toDoList.push(newTask);
@@ -60,30 +80,15 @@ addButton.addEventListener("click", () => {
   newRemoveButton.innerHTML = "Ta bort";
   newP.innerHTML = newTask;
 
+  newRemoveButton.addEventListener("click", () => {
+    toDoList.splice(toDoList.indexOf(newTask), 1);
+    completedTasksList.push(newTask);
+    newLi.remove();
+    htmlForCompleted();
+  });
+
   newLiContainer.appendChild(newP);
   newLiContainer.appendChild(newRemoveButton);
   newLi.appendChild(newLiContainer);
   taskList.appendChild(newLi);
-
-  console.log(toDoList);
 });
-
-//skapar nya uppgifter som läggs till i att göra listan (och i array) när man klickar på lägg till knappen
-// addButton.addEventListener("click", () => {
-//   const newLi = document.createElement("li");
-//   const newP = document.createElement("p");
-//   const newButton = document.createElement("button");
-//   const newLiContainer = document.createElement("div");
-//   newButton.innerHTML = "Ta bort";
-//   newP.innerHTML = input.value;
-
-//   newLiContainer.appendChild(newP);
-//   newLiContainer.appendChild(newButton);
-//   newLi.appendChild(newLiContainer);
-//   taskList.appendChild(newLi);
-
-//   //tar bort uppgift och lägger den i avklarat listan när man klickar på ta bort knappen
-//   newButton.addEventListener("click", () => {
-//     console.log("du klickade");
-//   });
-// });
